1acbd19e51386ace10ecab9eea9e3e9f27e6fb46
rytst <r.saito0111@gmail.com>
2024-10-28 05:46:08 +0900

    Single variable

--- example/main.go
+++ example/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "github.com/rytst/gotorch"
+    . "github.com/rytst/gotorch"
 )
 
 func main() {
--- gotorch.go
+++ gotorch.go
@@ -1,7 +1,19 @@
 package gotorch
 
 import (
+//    "reflect"
+    "math"
+//    "errors"
 )
+
+
+
+/*
+* For numerical type
+*/
+// type Number interface {
+//     float64 | []float64
+// }
 
 
 /*
@@ -10,3 +22,21 @@ import (
 type Variable[T any] struct {
     Data T
 }
+
+
+/*
+* Forward method
+*/
+type I interface {
+    Forward()
+}
+
+
+type Square struct {}
+
+func (sqrt Square) Forward(X Variable[float64]) (Variable[float64], error) {
+    var y Variable[float64]
+    y.Data = math.Sqrt(X.Data)
+    return y, nil
+}
+
